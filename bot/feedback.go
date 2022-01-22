22ef2f6f9b3301ec6f232d8fbeaf84c1d0466500
Andrey Stolbovsky <andrey.stolbovsky@gmail.com>
2022-01-23 01:52:36 +0300

    Add position detection to feedback parsers

--- bot/feedback.go
+++ bot/feedback.go
@@ -8,6 +8,9 @@ import (
 var prNoSuchItem = "Здесь нет такого предмета."
 var prNoSuchItemInInventory = "У Вас в руках нет такого предмета."
 var itemEmptyMatcher = regexp.MustCompile(`(?ms)^В .* пусто.$`)
+var prCantBecauseSleeping = "Сделать это во сне, что ли?"
+var prCantBecauseSitting = "Для этого необходимо встать на ноги."
+var prCantBecauseResting = "Вы слишком расслаблены, и Вам сейчас не до этого."
 
 func (b *Bot) ParseFeedback(s string) (res []Event) {
 	if botutil.HasAnyLinePrefix(s, []string{prNoSuchItem, prNoSuchItemInInventory}) {
@@ -15,6 +18,15 @@ func (b *Bot) ParseFeedback(s string) (res []Event) {
 	}
 	if itemEmptyMatcher.Match([]byte(s)) {
 		res = append(res, EVENT_WATER_CONTAINER_EMPTY)
+	}
+	if botutil.HasAnyLinePrefix(s, []string{prCantBecauseResting}) {
+		b.Char.Position = POSITION_RESTING
+	}
+	if botutil.HasAnyLinePrefix(s, []string{prCantBecauseSitting}) {
+		b.Char.Position = POSITION_SITTING
+	}
+	if botutil.HasAnyLinePrefix(s, []string{prCantBecauseSleeping}) {
+		b.Char.Position = POSITION_SLEEPING
 	}
 
 	return
